1b947e25d97cf7fe155d66c83b9fc40072cf857e
drc <drccsadrs@gmail.com>
2024-10-05 23:00:29 +0800

    feat: add billboard_decade_end_hot_100 tags and url

--- src/music_chart_scraper_config.py
+++ src/music_chart_scraper_config.py
@@ -15,6 +15,13 @@ MUSIC_CHART_SCRAPER_CONFIG = {
             "artist": "span.c-label.a-no-trucate.a-font-primary-s"    # Artist name tag
         },
         "url": "https://www.billboard.com/charts/hot-100"
-    }
-    # You can add more chart configurations here from other sources
+    },
+     "billboard_decade_end_hot_100": {
+        "tags": {
+            "chart_item": "div.o-chart-results-list-row-container",   # Main container for chart rows
+            "title": "h3.c-title",                                    # Song title tag
+            "artist": "span.c-label.a-font-primary-s"                 # Artist name tag
+        },
+        "url": "https://www.billboard.com/charts/decade-end/hot-100"
+    } # You can add more chart configurations here from other sources
 }
